41996b21e091bb86f7d1bfd8ea56eb5d2ec7b322
George <George.Smith@academytrainee.com>
2019-08-16 12:51:07 +0100

    first commit

---
+++ src/main/java/com/qa/javaAssessment/Assessment.java
@@ -0,0 +1,228 @@
+﻿package com.qa.javaAssessment;
+
+public class Assessment {
+	
+	public static void main(String[] args) {
+		
+		Assessment ass1 = new Assessment();
+		
+		System.out.println((ass1.multChar("The")));
+		System.out.println((ass1.getBert("xxbertfridgebertyy")));
+		System.out.println(ass1.evenlySpaced(4, 6, 3));
+		System.out.println(ass1.largest("55 72 86"));
+		System.out.println(ass1.fizzBuzz(15));
+		
+	}
+
+	// Given a string, return a string where
+	// for every char in the original string,
+	// there are three chars.
+
+	// multChar("The") ==> "TTThhheee"
+	// multChar("AAbb") ==> "AAAAAAbbbbbb"
+	// multChar("Hi-There") ==> "HHHiii---TTThhheeerrreee"
+
+	public String multChar(String input) {
+		String word = "";
+		for (int i = 0; i< input.length(); i++) {
+			word += input.substring(i, i+1);
+			word += input.substring(i, i+1);
+			word += input.substring(i, i+1);
+		}
+		return word;
+	}
+	
+	// Return the string (backwards) that is between the first and last appearance
+	// of "bert"
+	// in the given string, or return the empty string "" if there is not 2
+	// occurances of "bert" - Ignore Case
+
+	// getBert("bertclivebert") ==> "evilc"
+	// getBert("xxbertfridgebertyy") ==> "egdirf"
+	// getBert("xxBertfridgebERtyy") ==> "egdirf"
+	// getBert("xxbertyy") ==> ""
+	// getBert("xxbeRTyy") ==> ""
+
+	public String getBert(String input) {
+		
+		String returnSentence = "";
+		
+		int bertIndex1 = input.indexOf("bert");
+		int bertIndex2 = input.lastIndexOf("bert");
+		
+		if(bertIndex1 != -1) {
+			
+			for(int i = bertIndex1 + 4; i <= bertIndex2 - 1; i++) {
+				returnSentence += input.substring(i, i + 1);
+			}
+		
+		}	
+		char [] returnArray = returnSentence.toCharArray();
+		
+	
+	
+		return returnSentence;
+		
+	}
+
+	// Given three ints, a b c, one of them is small, one is medium and one is
+	// large. Return true if the three values are evenly spaced, so the
+	// difference between small and medium is the same as the difference between
+	// medium and large. Do not assume the ints will come to you in a reasonable
+	// order.
+
+	// evenlySpaced(2, 4, 6) ==> true
+	// evenlySpaced(4, 6, 2) ==> true
+	// evenlySpaced(4, 6, 3) ==> false
+	// evenlySpaced(4, 60, 9) ==> false
+
+	public boolean evenlySpaced(int a, int b, int c) {
+		
+		int [] numberArray = {a,b,c};
+		int [] sortedArray = {a,b,c};
+		
+		if(numberArray[0] < numberArray[1] && numberArray[0] < numberArray[2] ) {
+			sortedArray[0] = numberArray[0];
+		}else if(numberArray[0] < numberArray[1] && numberArray[0] > numberArray[2]) {
+			sortedArray[0] = numberArray[2];
+			sortedArray[2] = numberArray[0];
+		}else if(numberArray[0] > numberArray[1] && numberArray[0] < numberArray[2]) {
+			sortedArray[0] = numberArray[1];
+			sortedArray[1] = numberArray[0];
+		}
+		
+		
+		if(sortedArray[1] > sortedArray[0] && sortedArray[1] > sortedArray[2]) {
+			int temp = sortedArray[2];
+			sortedArray[2] = sortedArray[1];
+			sortedArray[1] = temp;
+		}
+		
+		if(sortedArray[1] - sortedArray[0] == sortedArray[2] - sortedArray[1]) {
+			return true;
+		}else {
+			return false;
+		}
+		
+		
+
+	}
+
+	// Given a string and an int n, return a string that removes n letters from the 'middle' of the string.
+	// The string length will be at least n, and be odd when the length of the input is odd.
+
+	// nMid("Hello", 3) ==> "Ho"
+	// nMid("Chocolate", 3) ==> "Choate"
+	// nMid("Chocolate", 1) ==> "Choclate"
+
+	public String nMid(String input, int a) {
+
+		
+		
+		return returnSentence;
+	}
+
+
+	// Given a string, return the length of the largest "block" in the string.
+	// A block is a run of adjacent chars that are the same.
+	//
+	// superBlock("hoopplla") ==> 2
+	// superBlock("abbCCCddDDDeeEEE") ==> 3
+	// superBlock("") ==> 0
+
+	public int superBlock(String input) {
+	
+
+		return -1;
+
+	}
+	
+	//given a string - return the number of times "am" appears in the String ignoring case -
+	// BUT ONLY WHEN the word "am" appears without being followed or proceeded by other letters
+	//
+	//amISearch("Am I in Amsterdam") ==> 1
+	//amISearch("I am in Amsterdam am I?") ==> 2
+	//amISearch("I have been in Amsterdam") ==> 0
+
+	public int amISearch(String arg1) {
+
+		String returnSentence = "";
+		int [] indexCounter = new int[arg1.length()];
+		
+		
+		indexCounter[0] = arg1.indexOf("am");
+		
+		if(indexCounter[0] != -1) {
+			
+		}
+		
+		
+	
+		return 0;
+		
+	}
+	
+	//given a number 
+	// if this number is divisible by 3 return "fizz"
+	// if this number is divisible by 5 return "buzz"
+	// if this number is divisible by both 3  and 5return "fizzbuzz"
+	//
+	//fizzBuzz(3) ==> "fizz"
+	//fizzBuzz(10) ==> "buzz"
+	//fizzBuzz(15) ==> "fizzbuzz"
+	
+	public String fizzBuzz(int arg1) {
+		
+		String word = "";
+
+		
+		if(arg1 % 3 == 0) {
+			word = "fizz";
+		}else if(arg1 % 5 == 0) {
+			word =  "buzz";
+		}
+		
+		
+		if(arg1 % 3 == 0 && arg1 % 5 == 0) {
+			word ="fizzbuzz";
+		
+		}
+		
+		return word;
+		
+	}
+	
+	
+	//Given a string split the string into the individual numbers present
+	//then add each digit of each number to get a final value for each number
+	// String example = "55 72 86"
+	//
+	// "55" will = the integer 10
+	// "72" will = the integer 9
+	// "86" will = the integer 14
+	//
+	// You then need to return the highest vale
+	//
+	//largest("55 72 86") ==> 14
+	//largest("15 72 80 164") ==> 11
+	//largest("555 72 86 45 10") ==> 15
+	
+	public int largest(String arg1) {
+		
+		String [] numbers = arg1.split(" ");
+		
+		for(int i = 0; i < numbers.length; i++) {
+			char [] charNumb = numbers[i].toCharArray();
+			char num1 = charNumb[0];
+			char num2 = charNumb[1];
+			
+			
+			
+			int num3 = num1 + num2;
+			System.out.println(num3);
+			
+		}
+		
+		return -1;
+	}
+}
